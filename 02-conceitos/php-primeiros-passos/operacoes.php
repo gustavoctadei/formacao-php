baf248b947dcd77786628673304e7666f4dd88b4
Gustavo Tadei <gustavo.tadei@trt14.jus.br>
2024-02-19 15:06:59 -0400

    commit inicial

---
+++ 02-conceitos/php-primeiros-passos/operacoes.php
@@ -0,0 +1,19 @@
+<?php
+
+$idade = 25;
+$idadeDaquiDezAnos = $idade + 10;
+$idadeHaCincoAnos = $idade - 5;
+
+echo $idadeDaquiDezAnos;
+echo $idadeHaCincoAnos;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 3;
+echo $doisAoCubo;
+
+$restoDaDivisao = 10 % 3;
+echo $restoDaDivisao;
---
+++ 02-conceitos/php-primeiros-passos/tipos.php
@@ -0,0 +1,19 @@
+<?php
+
+$idade = 21;
+//echo gettype($idade);
+
+$salario = 1000.301;
+//echo gettype($salario);
+
+$divisao = 10 / 3;
+// echo gettype($divisao);
+//echo $divisao;
+
+$mensagem = "Olá mundo";
+//echo gettype($mensagem);
+
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($verdadeiro);
