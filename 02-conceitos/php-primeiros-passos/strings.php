b987e720b2d1b4d3b8edde89b832bc7af1f2c174
Gustavo Tadei <gustavo.tadei@trt14.jus.br>
2024-02-26 13:42:49 -0400

    att

---
+++ 02-conceitos/php-primeiros-passos/strings.php
@@ -0,0 +1,21 @@
+<?php
+
+$idade = 28;
+//echo 'Olá Mundo! Minha idade é ' . $idade . ' anos.';
+
+//echo "Olá Mundo! Minha idade é $idade anos.";
+
+echo "Olá Mundo!\n";
+echo "\t";
+echo "Eu tenho $idade anos.\n\n";
+
+echo "Olá mundo!" . PHP_EOL;
+echo "Eu tenho $idade anos.";
+
+echo PHP_EOL . PHP_EOL;
+
+echo "Você só pode entrar se tiver mais de 18 anos\n";
+
+$idade = 16;
+
+echo "Você tem $idade anos. Pode entrar.";
